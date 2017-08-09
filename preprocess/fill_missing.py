b79021abf58d90dd5eb73f515a8923633df6d8ba
Rachel White <rhwhite32@gmail.com>
2017-08-09 14:33:23 -0700

    tidying up

---
+++ preprocess/fill_missing.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import os, errno
+import numpy as np
+import numpy.ma as ma
+import netCDF4
+from netCDF4 import Dataset
+import datetime as dt
+import re
+import sys
+
+import xray
+
+#Thresholds are in mm/day, data is in mm/hr
+
+
+startyr = 1998
+endyr = 2014
+
+
+#FileP = 'TRMM_pcp_3hrly_nonan2.2005.nc'
+FileP = 'TRMM_' + str(startyr) + '-' + str(endyr) + '_3B42_3hrly_nonan2.nc'
+
+minevent = 100000
+
+#DirP = '/home/disk/eos4/rachel/Obs/TRMM/3hrly/'
+DirP = '/home/disk/eos4/rachel/Obs/TRMM/3hrly/raw/'
+
+precipdata = Dataset(DirP + FileP,'r+')
+
+ntimes = len(precipdata['time'])
+
+print(ntimes)
+print "start looking at missing values"
+
+# for first timestep, can't fill missing values
+step = 4000
+
+count = 0
+for nt in range(0,ntimes,step):
+	print nt
+	upto = min(nt + step+2,ntimes)
+	print upto
+	precipin = ma.getdata(precipdata.variables['pcp'][nt:upto,:,:])
+
+	mvs = np.where(precipin < -1)
+
+	nvalues = len(mvs[0])
+	print nvalues
+	for nv in range(0,nvalues):
+        if mvs[0][nv]+count == 0 or mvs[0][nv]+count == ntimes-1:
+            #can't do anything for missing values at the very beginning or very end of the timeseries
+            #print "can't deal with missing values sequentially in time, setting to zero"
+            precipin[mvs[0][nv],mvs[1][nv],mvs[2][nv]] = 0.0
+
+		elif mvs[0][nv] < step+1:
+            if (precipin[mvs[0][nv]-1,mvs[1][nv],mvs[2][nv]] > -1 and precipin[mvs[0][nv]+1,mvs[1][nv],mvs[2][nv]] > -1):
+                    precipin[mvs[0][nv],mvs[1][nv],mvs[2][nv]] = 0.5 * precipin[mvs[0][nv]-1,mvs[1][nv],mvs[2][nv]] + \
+                        0.5 * precipin[mvs[0][nv]+1,mvs[1][nv],mvs[2][nv]]
+			else:
+                precipin[mvs[0][nv],mvs[1][nv],mvs[2][nv]] = 0.0
+		else:
+			#don't do anything for the last timestamp in each segment - it will be included in the next unless it's the
+			#last one in which case we've already dealt with it!
+			print "not doing anything for time " + str(count + mvs[0][nv])
+
+	count = count + step
+
+	mvs2 = np.where(precipin[0:step+1,:,:] < -1)
+	nvalues2 = len(mvs2[0])
+	if nvalues2 != 0:
+        sys.err("error with filling missing data; missing data remains")
+
+	precipdata.variables['pcp'][nt:upto,:,:] = precipin
+	print 'finished filling in missing data'
+
+
--- preprocess/thresholding.py
+++ preprocess/thresholding.py
@@ -48,88 +48,106 @@ endyr = args.endyr[0]
 
 nyears = endyr-startyr + 1
 
-if Data == "TRMM":
-	if name == "5th_from48_":
-		Threshold = [48,72,96,120,168]
-	elif name == "Standard":
-		Threshold = [24, 40, 56, 80, 120]
-	elif name == "6th_from6":
-		Threshold = [6,12,24,48,72,96]
-elif Data == "TRMMERAIgd":
-	if name == "Standard": 
-		Threshold = [24, 40, 56, 80, 120]
-	elif name == "2mmhr":
-		Threshold = [48,72,96,120,148]
-	else:
-		exit("unknown name " + name)
-elif Data == "ERAI":
-	if name == "Standard":
-		Threshold = [24, 40, 56, 80, 120]
-	elif name == "low":
-		Threshold = [12, 18, 26, 36, 48]
-        elif name == "2mmhr":
-                Threshold = [48,72,96,120,148]
-	elif name == "vlow":
-		Threshold = [2, 4, 8, 16, 32, 48]
-	else:
-		exit("unknown name " + name)
-elif Data == "ERA20C":
-	if name == "20Cstd":
-                Threshold = [4,8,16,32,64,128]
-        elif name == "std":
-                Threshold = [12,24,48,72,96]
+#Defaults
+latvar = 'latitude'
+lonvar = 'longitude'
+Dir = '/home/disk/eos4/rachel/Obs/' + Data + '/'
+DirO = Dir + 'Thresholds/'
 
-	else:
-		exit("unknown name" + name)
+# Set dataset specific variables:
+if Data == "TRMM":
+    Filein = 'TRMM_1998-2014_3B42_3hrly_nonan.nc'
+    precipvar = 'pcp'
+    precipunit = 'mm/hr'
+    if name == "5th_from48_":
+        Threshold = [48,72,96,120,168]
+    elif name == "Standard":
+        Threshold = [24, 40, 56, 80, 120]
+    elif name == "6th_from6":
+        Threshold = [6,12,24,48,72,96]
+elif Data == "TRMMERAIgd":
+    Dir = '/home/disk/eos4/rachel/Obs/TRMM/'
+    DirO = '/home/disk/eos4/rachel/Obs/TRMM/Thresholds/ERAIgd/'
+    Filein = "regrid2ERAI_TRMM_3B42_1998-2014.nc"
+    precipvar = 'pcp'
+    precipunit = 'mm/hr'
+
+    if name == "Standard": 
+        Threshold = [24, 40, 56, 80, 120]
+    elif name == "2mmhr":
+        Threshold = [48,72,96,120,148]
+    else:
+        exit("unknown name " + name)
+elif Data == "ERAI":
+    Dir = '/home/disk/eos4/rachel/Obs/ERAI/Precip_3hrly/'
+    DirO = '/home/disk/eos4/rachel/Obs/ERAI/Precip_3hrly/Thresholds/'
+    Filein = 'ERAI_Totalprecip_1980-2015_preprocess.nc'
+    precipvar = 'tpnew'
+    precipunit = 'mm/hr'
+
+    if name == "Standard":
+        Threshold = [24, 40, 56, 80, 120]
+    elif name == "low":
+        Threshold = [12, 18, 26, 36, 48]
+    elif name == "2mmhr":
+        Threshold = [48,72,96,120,148]
+    elif name == "vlow":
+        Threshold = [2, 4, 8, 16, 32, 48]
+    else:
+        exit("unknown name " + name)
+elif Data == "ERA20C":
+    Filein = 'ERA_20C_Totalprecip_' + str(startyr) + '-' + str(endyr) + '_preprocess.nc'
+    precipvar = 'tpnew'
+    precipunit = 'mm/hr'
+    if name == "20Cstd":
+        Threshold = [4,8,16,32,64,128]
+    elif name == "std":
+        Threshold = [12,24,48,72,96]
+
+    else:
+        exit("unknown name" + name)
+elif Data == "GPCP":
+    Dir = '/home/disk/eos4/rachel/Obs/GPCP/Daily/'
+    DirO = '/home/disk/eos4/rachel/Obs/GPCP/Daily/Thresholds/'
+    Filein = 'GPCP_1DD_v1.2_199610-201510.nc'
+    precipvar = 'PREC'
+    precipunit = 'mm/day'
+    lonvar = 'lon'
+    latvar = 'lat'
+
+    if name == "Daily12":
+        Threshold = [12, 20, 28, 40, 60]
+    else:
+        exit("unknown name " + name)
 else:
         exit("unknown data type " + Data)
 
-nThresh = len(Threshold)
-Thresh = [x/24.0 for x in Threshold]
-
-print Thresh
-threshlow = Thresh[0]
-if Data == "TRMM":
-        Dir = '/home/disk/eos4/rachel/Obs/TRMM/'
-        DirO = '/home/disk/eos4/rachel/Obs/TRMM/Thresholds/'
-	Filein = 'TRMM_1998-2014_3B42_3hrly_nonan.nc'
-
-elif Data == "TRMMERAIgd":
-        Dir = '/home/disk/eos4/rachel/Obs/TRMM/'
-        DirO = '/home/disk/eos4/rachel/Obs/TRMM/Thresholds/ERAIgd/'
-        Filein = "regrid2ERAI_TRMM_3B42_1998-2014.nc"
-
-elif Data == "ERAI":
-	Dir = '/home/disk/eos4/rachel/Obs/ERAI/Precip_3hrly/'
-	DirO = '/home/disk/eos4/rachel/Obs/ERAI/Precip_3hrly/Thresholds/'
-	Filein = 'ERAI_Totalprecip_1980-2015_preprocess.nc'
-
-elif Data == "ERA20C":
-	Dir = '/home/disk/eos4/rachel/Obs/ERA_20C/'
-	DirO = '/home/disk/eos4/rachel/Obs/ERA_20C/Thresholds/'
-	Filein = 'ERA_20C_Totalprecip_' + str(startyr) + '-' + str(endyr) + '_preprocess.nc'
-
-else:
-	exit("unexpected Data type " + Data)
-
-
+# Make output directory if it doesn't already exist
 try:
-	os.mkdir(DirO + '/' + name + '_' + str(startyr) + '-' + str(endyr))
+    os.mkdir(DirO + '/' + name + '_' + str(startyr) + '-' + str(endyr))
 except OSError:
-	pass
+    pass
 
 DirO = DirO + '/' + name + '_' + str(startyr) + '-' + str(endyr) + '/'
 
+# Convert thresholds if necessary
+
+nThresh = len(Threshold)
+if precipunit == 'mm/day':
+    Thresh = Threshold
+elif precipunit == 'mm/hr':
+    Thresh = [x/24.0 for x in Threshold]    # convert to mm/hr
+else:
+    exit("unexpected precipunit " + precipunit)
+
+
+# Read in precip data
+print Dir + Filein
 precipin = xrayOpen(Dir + Filein)
 
-latsin = precipin['latitude'][:]
-lonsin = precipin['longitude'][:]
-if Data in ["TRMM","TRMMERAIgd"]:
-	precipnew=precipin['pcp']
-elif Data in ["ERAI","ERA20C"]:
-	precipnew=precipin['tpnew']
-
-#print precipnew.time
+latsin = precipin[latvar][:]
+lonsin = precipin[lonvar][:]
+precipnew=precipin[precipvar]
 
 nlons = len(lonsin)
 nlats = len(latsin)
@@ -147,30 +165,30 @@ missed = []
 startTime = time.time()
 
 try:
-	precipchunk = precipnew.values
-        total = np.nansum(precipchunk)
-	missedvalues = np.ma.sum(np.ma.masked_greater_equal(precipchunk[np.isfinite(precipchunk)],Thresh[0]))
-	totals.append(total)
-	missed.append(missedvalues)
+    precipchunk = precipnew.values
+    total = np.nansum(precipchunk)
+    missedvalues = np.ma.sum(np.ma.masked_greater_equal(precipchunk[np.isfinite(precipchunk)],Thresh[0]))
+    totals.append(total)
+    missed.append(missedvalues)
 
 except MemoryError:
-	print 'memory error, doing in chunks'
-	for attempt in range(10):
-		try:
-			nits = np.ceil(ntimes/chunksize)
-			print str(nits) + "iterations required"
-			for i in range(0,np.int(nits)):
-				print i
-				precipchunk = np.float32(precipnew.isel(time=slice(i*chunksize,np.amin([(i+1)*chunksize,ntimes]))))
-				total = np.nansum(precipchunk)
-				missedvalues = np.ma.sum(np.ma.masked_greater_equal(precipchunk[np.isfinite(precipchunk)],Thresh[0]))
-				totals.append(total)
-				missed.append(missedvalues)
-		except MemoryError:
-			chunksize = chunksize/2
-			print chunksize
-			continue
-		break
+    print 'memory error, doing in chunks'
+    for attempt in range(10):
+        try:
+            nits = np.ceil(ntimes/chunksize)
+            print str(nits) + "iterations required"
+            for i in range(0,np.int(nits)):
+                print i
+                precipchunk = np.float32(precipnew.isel(time=slice(i*chunksize,np.amin([(i+1)*chunksize,ntimes]))))
+                total = np.nansum(precipchunk)
+                missedvalues = np.ma.sum(np.ma.masked_greater_equal(precipchunk[np.isfinite(precipchunk)],Thresh[0]))
+                totals.append(total)
+                missed.append(missedvalues)
+        except MemoryError:
+            chunksize = chunksize/2
+            print chunksize
+            continue
+        break
 
 
 
@@ -185,75 +203,77 @@ print missed
 print "this threshold misses " + str(sum(missed)) + " which is " + str(totalmissed) + " % of the total rain"
 
 if check:
-	accept = query_yes_no("is this acceptable?")
-	if not accept: exit("you didn't accept the thresholds")
+    accept = query_yes_no("is this acceptable?")
+    if not accept: exit("you didn't accept the thresholds")
 
 timesin = precipin['time'][:]
 
 for year in range(startyr,endyr+1):    #(1998,2015)
-	print year
-	stryear = str(year)
-	counter = 0	#reset counter for each year
+    print year
+    stryear = str(year)
+    counter = 0 #reset counter for each year
 
-	precipchunk = precipnew.sel(time=str(year))
-	timeschunk = timesin.sel(time=str(year))
-	ntimes = len(timeschunk)
+    precipchunk = precipnew.sel(time=str(year))
+    timeschunk = timesin.sel(time=str(year))
+    ntimes = len(timeschunk)
 
-	for x in np.nditer(precipchunk, op_flags=['readwrite']):
-		if x < Thresh[0]:	# Deal with values below 1st threshold (0)
-			x[...] = 0
-		elif x >= Thresh[nThresh-1]:	# Deal with values above last threshold (nThresh - 1)
-			x[...] = nThresh
-		else:
-			for ith in range(0,nThresh-1):	# iterate through thresholds 0 to nThresh - 2
-				if x >= Thresh[ith] and x < Thresh[ith+1]:
-					x[...] = ith + 1
-					break
+    for x in np.nditer(precipchunk, op_flags=['readwrite']):
+        if x < Thresh[0]:   # Deal with values below 1st threshold (0)
+            x[...] = 0
+        elif x >= Thresh[nThresh-1]:    # Deal with values above last threshold (nThresh - 1)
+            x[...] = nThresh
+        else:
+            for ith in range(0,nThresh-1):  # iterate through thresholds 0 to nThresh - 2
+                if x >= Thresh[ith] and x < Thresh[ith+1]:
+                    x[...] = ith + 1
+                    break
 
-#		for y in np.nditer(precip_thrsh):
-#		    if y > 5:
-#			print 'missing value'
-#			print y
-#		    elif y < 0:
-#			print 'weird value'
-#			print y
+#       for y in np.nditer(precip_thrsh):
+#           if y > 5:
+#           print 'missing value'
+#           print y
+#           elif y < 0:
+#           print 'weird value'
+#           print y
 
-	maxcount = ntimes
-	print maxcount
-	for count in range(0,maxcount):
-		filecount = str(counter)
-		if Data == "TRMM":
-			FileO = 'TRMM_3B42_3hrly_' + name + stryear + '_' + filecount.zfill(5) + '.nc'
-		elif Data == "TRMMERAIgd":
+    maxcount = ntimes
+    print maxcount
+    for count in range(0,maxcount):
+        filecount = str(counter)
+        if Data == "TRMM":
+            FileO = 'TRMM_3B42_3hrly_' + name + stryear + '_' + filecount.zfill(5) + '.nc'
+        elif Data == "TRMMERAIgd":
             FileO = 'TRMMERAIgd_3B42_3hrly_' + name + stryear + '_' + filecount.zfill(5) + '.nc'
-		elif Data == "ERAI":
+        elif Data == "ERAI":
             FileO = 'ERAI_3hrly_' + name + "_" + stryear + '_' + filecount.zfill(5) + '.nc'
         elif Data == "ERA20C":
             FileO = 'ERA20C_3hrly_' + name + "_" + stryear + '_' + filecount.zfill(5) + '.nc'
+        elif Data == "GPCP":
+            FileO = 'GPCP_' + name + "_" + stryear + '_' + filecount.zfill(5) + '.nc'
 
-		try:
+        try:
             os.remove(DirO + FileO)
-		except OSError:
+        except OSError:
            pass
 
-		ncfile = Dataset(DirO + FileO, 'w')
-		ncfile.createDimension('z', 1)
-		ncfile.createDimension('y',len(latsin))
-		ncfile.createDimension('x',len(lonsin))
-		ncfile.createDimension('time',1)
+        ncfile = Dataset(DirO + FileO, 'w')
+        ncfile.createDimension('z', 1)
+        ncfile.createDimension('y',len(latsin))
+        ncfile.createDimension('x',len(lonsin))
+        ncfile.createDimension('time',1)
 
-		precipTH = ncfile.createVariable('value','f4',('z','y','x'),fill_value=0)
+        precipTH = ncfile.createVariable('value','f4',('z','y','x'),fill_value=0)
 
-		precipTH[:,:,:] = precipchunk[count,:,:].values
-		precipTH.grid_type = 'linear'
-		precipTH.grid_name = 'grid-1'
+        precipTH[:,:,:] = precipchunk[count,:,:].values
+        precipTH.grid_type = 'linear'
+        precipTH.grid_name = 'grid-1'
 
 
-		#global attribute
-		ncfile.Conventions = 'COARDS'
-		ncfile.calendar = 'standard'
-		ncfile.center = 'gsfc'
-		ncfile.Thresholds = 'Thresholds: ' + str(Threshold)
-		ncfile.Missed = 'missed by thresholds: ' + str(totalmissed) + '% of total rain'
-		ncfile.close()
-		counter = counter + 1
+        #global attribute
+        ncfile.Conventions = 'COARDS'
+        ncfile.calendar = 'standard'
+        ncfile.center = 'gsfc'
+        ncfile.Thresholds = 'Thresholds: ' + str(Threshold)
+        ncfile.Missed = 'missed by thresholds: ' + str(totalmissed) + '% of total rain'
+        ncfile.close()
+        counter = counter + 1
